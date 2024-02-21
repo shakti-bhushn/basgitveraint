c1a1348ff2d9e6c8f45ae90e705bfa7f8310b2ce
SHAKTI PANDEY <shaktiit2011@gmail.com>
2024-02-21 14:56:31 +0000

    Initial Commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("com.fiori.integration.basgitveracodeintegration1.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comfioriintegration/basgitveracodeintegration1/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
